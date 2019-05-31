7c6b8f02df9029d093c3964965f4b6ea7fd938e8
Joana Martins <joanalm@gmail.com>
2019-05-31 18:14:50 +0100

    Update

--- QRS_Detector/download_data.sh
+++ QRS_Detector/download_data.sh
@@ -1,15 +1,22 @@
-output_folder=/home/lucas/Projects/ADNE_Project/QRS_Detector/data
+output_folder=/home/lucas/Projects/ADNE_Project/QRS_Detector/data/Test
 
 for i in $(seq 1 9)
 do
-	wget https://physionet.org/pn3/incartdb/I0$i.dat -P $output_folder
-	wget https://physionet.org/pn3/incartdb/I0$i.hea -P $output_folder
-	wget https://physionet.org/pn3/incartdb/I0$i.atr -P $output_folder
+	wget https://physionet.org/pn3/incartdb/10$i.dat -P $output_folder
+	wget https://physionet.org/pn3/incartdb/10$i.hea -P $output_folder
+	wget https://physionet.org/pn3/incartdb/10$i.atr -P $output_folder
 done
 
-for j in $(seq 10 75)
+for j in $(seq 10 99)
 do
-	wget https://physionet.org/pn3/incartdb/I$j.dat -P $output_folder
-        wget https://physionet.org/pn3/incartdb/I$j.hea -P $output_folder
-        wget https://physionet.org/pn3/incartdb/I$j.atr -P $output_folder
+	wget https://physionet.org/pn3/incartdb/1$j.dat -P $output_folder
+        wget https://physionet.org/pn3/incartdb/1$j.hea -P $output_folder
+        wget https://physionet.org/pn3/incartdb/1$j.atr -P $output_folder
+done
+
+for j in $(seq 100 234)
+do
+	wget https://physionet.org/pn3/incartdb/$j.dat -P $output_folder
+        wget https://physionet.org/pn3/incartdb/$j.hea -P $output_folder
+        wget https://physionet.org/pn3/incartdb/$j.atr -P $output_folder
 done
